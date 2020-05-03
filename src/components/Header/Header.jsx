8fa04f70133cd81e0d9bb01eb36173b03f4c724d
dae-hyun-kim <daehk4@uci.edu>
2020-05-03 19:48:17 +0000

    logo added - Dae Kim

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+import logo from '../../assets/valorant-logo.png'
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-        <div className={styles.mainHead}>Hello</div>
+        <div className={`d-flex align-items-center ${styles.mainHead}`}>
+          <div className="col-6">
+            <img src={logo} alt="" className={styles.logo}/>
+          </div>
+          <div className="col-6">
+
+          </div>
+        </div>
       </header>
     )
   }
