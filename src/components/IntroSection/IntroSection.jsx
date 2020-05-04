0916de5c1a83ae0c4872156bad597138c6875f7b
dae-hyun-kim <daehk4@uci.edu>
2020-05-04 20:47:28 +0000

    created intro section component - Dae Kim

---
+++ src/components/IntroSection/IntroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./IntroSection.jsx";
+
+class IntroSection extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
