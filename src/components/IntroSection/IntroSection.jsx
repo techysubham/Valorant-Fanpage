2607423e5a39ef18ccd9821f4305f342a8c64c93
dae-hyun-kim <daehk4@uci.edu>
2020-05-04 21:44:39 +0000

    added intro box - Dae Kim

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -8,6 +8,11 @@ class IntroSection extends React.Component {
       <div className={`d-flex justify-content-center align-items-center ${styles.introSectionBackground}`}>
         <div className={styles.logoContainer}>
           <img src={logo} alt="" className={`${styles.slideIn} ${styles.valorantLogo}`}/>
+          <div className="d-flex justify-content-center">
+            <div className={`${styles.test} ${styles.slideIn}`}>
+              <h5>A 5v5 character-based tactical shooter</h5>
+            </div>
+          </div>
         </div>
       </div>
     )
