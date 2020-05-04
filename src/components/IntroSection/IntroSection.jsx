fc83cd19e4b26440992f240d57c13f80d9b7aceb
dae-hyun-kim <daehk4@uci.edu>
2020-05-04 21:34:07 +0000

    valorant word logo added - Dae Kim

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./IntroSection.module.css";
+import logo from '../../assets/valorant.png'
 
 class IntroSection extends React.Component {
   render() {
     return (
-      <div className={styles.introSectionBackground}>
+      <div className={`d-flex justify-content-center align-items-center ${styles.introSectionBackground}`}>
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="" className={`${styles.slideIn} ${styles.valorantLogo}`}/>
+        </div>
       </div>
     )
   }
