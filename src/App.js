e3c1deb252d220038b9b622c336b10de6c549f96
dae-hyun-kim <daehk4@uci.edu>
2020-05-05 18:35:33 +0000

    footer component created - Dae Kim

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react"
 import Header from "./components/Header/Header"
 import IntroSection from "./components/IntroSection/IntroSection"
+import Footer from "./components/Footer/Footer"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screen : null
+    }
+  }
   render() {
     return (
       <div>
         <Header/>
         <IntroSection/>
+        <Footer/>
       </div>
     )
   }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className={`${styles.mainFooter}`}>
+
+        </div>
+      </footer>
+    )
+  }
+}
+
+export default Footer
