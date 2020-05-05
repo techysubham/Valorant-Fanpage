68fe96c0914d15faff3153df867d3edb26636f85
dae-hyun-kim <daehk4@uci.edu>
2020-05-05 18:50:09 +0000

    added riot logo - Dae Kim

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./Footer.module.css";
+import fistLogo from "../../assets/fistLogo.png"
 
 class Footer extends React.Component {
   render() {
     return (
       <footer>
         <div className={`${styles.mainFooter}`}>
-
+          <div>
+            <img src={fistLogo} alt="" className={styles.fistLogoSize}/>
+          </div>
         </div>
       </footer>
     )
