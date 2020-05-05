54375664286183f5b9baeaca8f15cd527ffd6079
dae-hyun-kim <daehk4@uci.edu>
2020-05-05 19:14:22 +0000

    main page styling complete - Dae Kim

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import fistLogo from "../../assets/fistLogo.png"
+import fistLogo from "../../assets/fistLogo.png";
+import rating from "../../assets/rating.png";
 
 class Footer extends React.Component {
   render() {
@@ -12,7 +13,17 @@ class Footer extends React.Component {
               <img src={fistLogo} alt="" className={`mt-4 ${styles.fistLogoSize}`}/>
             </div>
             <div>
-              <p className={`d-flex justify-content-center ${styles.footerNote}`}>Notice: This is a fake Valorant game website. I do not have any business with Riot Games.</p>
+              <p className={`d-flex justify-content-center ${styles.footerNote}`}>Notice: This is a fake Valorant game website and is only a personal project. I do not have any business with Riot Games.</p>
+            </div>
+            <div className="d-flex justify-content-center mt-4">
+              <img src={rating} alt="" className={styles.ratingSizing}/>
+              <div className="ml-2">
+                <p className={styles.ratingDetails}>Blood</p>
+                <p className={styles.ratingDetails}>Language</p>
+                <p className={styles.ratingDetails}>Violence</p>
+                <p className={styles.ratingDetails}>User Interact</p>
+                <p className={styles.ratingDetails}>In-Game Purchases</p>
+              </div>
             </div>
           </div>
         </div>
