faf1b1b77aa201df4ce843ecef294b2439bbd694
dae-hyun-kim <daehk4@uci.edu>
2020-05-06 22:21:06 +0000

    added explanation text to section one - Dae Kim

--- src/components/GameInfoSectionOne/GameInfoSectionOne.jsx
+++ src/components/GameInfoSectionOne/GameInfoSectionOne.jsx
@@ -9,6 +9,9 @@ class GameInfoSectionOne extends React.Component {
         <div className="d-flex justify-content-around">
           <div className="col-4 ml-4">
             <h2 className={`${styles.slideIn} ${styles.topPadding} ${styles.bigFont}`}>DOMINATE YOUR FOES WITH PRECISION SHOOTING...  </h2>
+            <div className="mt-5 col-9">
+              <p className={`${styles.smallFont} ${styles.fadeIn}`}>Whether you enjoy sniping from long distances, dueling with rifles and SMGs, or blowing enemies away with shotguns, Valorant has a wide array of firearms to fit your playstyle.</p>
+            </div>
           </div>
           <div className="col-5">
             <img className={`${styles.shootingGifSize} ${styles.topPadding}`} src={shootingGif} alt=""/>
