a6528c5c0d8c2065aeea0c0dcb78b321bf0cfc29
dae-hyun-kim <daehk4@uci.edu>
2020-05-06 22:35:26 +0000

    added explanation text to section two - Dae Kim

--- src/components/GameInfoSectionTwo/GameInfoSectionTwo.jsx
+++ src/components/GameInfoSectionTwo/GameInfoSectionTwo.jsx
@@ -12,6 +12,9 @@ class GameInfoSectionTwo extends React.Component {
           </div>
           <div className="col-4">
             <h2 className={` ${styles.slideIn} ${styles.topPadding} ${styles.bigFont}`}>...OR WITH STRATEGIC ABILITIES  </h2>
+            <div className="mt-5 col-11">
+              <p className={`${styles.smallFont} ${styles.fadeIn}`}>Valorant has 10 agents, all with unique and specialized abilities to aid in the most intense battles, and allowing players to be creative and strategic with their approach to success.</p>
+            </div>
           </div>
         </div>
       </div>
