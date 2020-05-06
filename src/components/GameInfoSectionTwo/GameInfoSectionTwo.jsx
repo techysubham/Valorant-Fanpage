3864c33b7122b6870d899cdfa522b6a6db8e17f0
dae-hyun-kim <daehk4@uci.edu>
2020-05-06 22:03:57 +0000

    section two complete - Dae Kim

--- src/components/GameInfoSectionTwo/GameInfoSectionTwo.jsx
+++ src/components/GameInfoSectionTwo/GameInfoSectionTwo.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import styles from "./GameInfoSectionTwo.module.css";
+import abilitiesGif from "../../assets/abilities.gif"
 
 class GameInfoSectionTwo extends React.Component {
   render() {
     return (
       <div className={styles.sectionOne}>
-        <div className="d-flex justify-content-around">
-          <div className="col-4 ml-4">
-            <h2 className={`${styles.slideIn} ${styles.topPadding} ${styles.bigFont}`}>DOMINATE YOUR FOES WITH PRECISION SHOOTING...  </h2>
+        <div className="d-flex justify-content-center">
+          <div className="col-6">
+            <img src={abilitiesGif} alt="" className={`${styles.shootingGifSize} ${styles.topPadding}`}/>
           </div>
-          <div className="col-5">
-
+          <div className="col-4">
+            <h2 className={` ${styles.slideIn} ${styles.topPadding} ${styles.bigFont}`}>...OR WITH STRATEGIC ABILITIES  </h2>
           </div>
         </div>
       </div>
