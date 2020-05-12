50629392183c1b492f0da379fdeb6e7a7c1d24f0
dae-hyun-kim <daehk4@uci.edu>
2020-05-12 00:32:52 +0000

    created agents component - Dae Kim

---
+++ src/components/Agents/Agents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import styles from "./Agents.module.css";
+
+class Agents extends React.Component {
+  render() {
+    return (
+      <div>hello</div>
+    )
+  }
+}
+
+export default Agents
