295743c12c9bbff64fcd575d26b6f1ee406c54aa
dae-hyun-kim <daehk4@uci.edu>
2020-05-12 00:45:49 +0000

    added all agent images - Dae Kim

--- src/components/Agents/Agents.jsx
+++ src/components/Agents/Agents.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-// import styles from "./Agents.module.css";
+import styles from "./Agents.module.css";
+import breach from "../../assets/breach.png";
+import brimstone from "../../assets/brimstone.png";
 
 class Agents extends React.Component {
   render() {
     return (
-      <div>hello</div>
+      <div className="container">
+        <div>
+          <img src={breach} alt="" className={styles.breachImage}/>
+          <img src={brimstone} alt="" className={styles.breachImage} />
+        </div>
+      </div>
     )
   }
 }
