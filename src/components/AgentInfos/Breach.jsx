56d0fc1a972b1a387595606efc00a3065f3d2e42
dae-hyun-kim <daehk4@uci.edu>
2020-05-13 19:51:14 +0000

    breach component template added - Dae Kim

--- src/components/AgentInfos/Breach.jsx
+++ src/components/AgentInfos/Breach.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-// import styles from "./AgentInfos.module.css";
+import styles from "./AgentInfos.module.css";
+import breach from "../../assets/breach.png";
 
 class Breach extends React.Component {
   render() {
     return (
-      <div>
-        hello
+      <div className={`${styles.background} ${styles.breachBackground} d-flex justify-content-around`}>
+        <div className="col-5">
+
+        </div>
+        <div className="col-5">
+          <img src={breach} alt="" className={`${styles.agentImages} ${styles.slideOverFromRight}`}/>
+        </div>
       </div>
     )
   }
