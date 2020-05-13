368357834ec7574104b434e9451a247d4996f974
dae-hyun-kim <daehk4@uci.edu>
2020-05-13 19:19:00 +0000

    added hover effect on agents - Da eKim

--- src/components/Agents/Agents.jsx
+++ src/components/Agents/Agents.jsx
@@ -16,45 +16,45 @@ class Agents extends React.Component {
     return (
       <div className={`d-flex justify-content-center ${styles.background}`}>
         <div className="col-12 d-flex flex-wrap justify-content-around">
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={breach} alt="" className={`${styles.agentImage} ${styles.breachContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>BREACH</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.breachContainer}`}>
+            <img src={breach} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>BREACH</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={brimstone} alt="" className={`${styles.agentImage} ${styles.brimstoneContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>BRIMSTONE</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.brimstoneContainer}`}>
+            <img src={brimstone} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>BRIMSTONE</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={cypher} alt="" className={`${styles.agentImage} ${styles.cypherContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>CYPHER</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.cypherContainer}`}>
+            <img src={cypher} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>CYPHER</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={jett} alt="" className={`${styles.agentImage} ${styles.jettContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>JETT</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.jettContainer}`}>
+            <img src={jett} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>JETT</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={omen} alt="" className={`${styles.agentImage} ${styles.omenContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>OMEN</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.omenContainer}`}>
+            <img src={omen} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>OMEN</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={phoenix} alt="" className={`${styles.agentImage} ${styles.phoenixContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>PHOENIX</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.phoenixContainer}`}>
+            <img src={phoenix} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>PHOENIX</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={raze} alt="" className={`${styles.agentImage} ${styles.razeContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>RAZE</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.razeContainer}`}>
+            <img src={raze} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>RAZE</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={sage} alt="" className={`${styles.agentImage} ${styles.sageContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>SAGE</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.sageContainer}`}>
+            <img src={sage} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>SAGE</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={sova} alt="" className={`${styles.agentImage} ${styles.sovaContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>SOVA</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.sovaContainer}`}>
+            <img src={sova} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>SOVA</h2>
           </div>
-          <div className={`col-3 m-4 ${styles.agentContainer}`}>
-            <img src={viper} alt="" className={`${styles.agentImage} ${styles.viperContainer}`} />
-            <h2 className={`text-center m-3 p-2 ${styles.agentName}`}>VIPER</h2>
+          <div className={`col-3 m-4 ${styles.agentContainer} ${styles.viperContainer}`}>
+            <img src={viper} alt="" className={`${styles.agentImage} ${styles.slideIn}`} />
+            <h2 className={`text-center m-3 p-2 ${styles.agentName} ${styles.slideIn}`}>VIPER</h2>
           </div>
         </div>
       </div>
