13007be7ca163ddec055521284085fe25fc251cf
dae-hyun-kim <daehk4@uci.edu>
2020-05-14 19:47:17 +0000

    sova and viper agent components completed - Dae Kim

--- src/components/AgentInfos/Sova.jsx
+++ src/components/AgentInfos/Sova.jsx
@@ -1,15 +1,49 @@
 import React from "react";
 import styles from "./AgentInfos.module.css";
 import sova from "../../assets/sova.png";
+import shockBolt from "../../assets/shockBolt.png";
+import owlDrone from "../../assets/owlDrone.png";
+import reconBolt from "../../assets/reconBolt.png";
+import huntersFury from "../../assets/huntersFury.png";
 
 class Sova extends React.Component {
   render() {
     return (
       <div className={`${styles.background} ${styles.sovaBackground} d-flex justify-content-around`}>
-        <div className={`col-5 mt-5 ${styles.slideOverFromLeft}`}>
+        <div className={`col-5 mt-2 ${styles.slideOverFromLeft}`}>
           <h1 className={`text-center ${styles.agentFont}`}>SOVA</h1>
           <h6 className={`text-center ${styles.agentInfoFont}`}>Sova tracks, finds, and eliminates enemies with ruthless efficiency and precision. His custom bow and incredible scouting
           abilities ensure that even if you run, you cannot hide.</h6>
+          <div className="mt-5">
+            <div className="d-flex">
+              <img src={shockBolt} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>SHOCK BOLT</h5>
+                <p className={`${styles.abilityDescription}`}>Equip a bow with shock bolt. Fire to send the explosive bolt forward detonating upon collision and damaging players nearby. Hold Fire to extend the range of the projectile. Alternate Fire to add up to two bounces to this arrow.</p>
+              </div>
+            </div>
+            <div className="d-flex mt-4">
+              <img src={owlDrone} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>OWL DRONE</h5>
+                <p className={`${styles.abilityDescription}`}>Equip an owl drone. Fire to deploy and take control of movement of the drone. While in control of the drone, Fire to shoot a marking dart. This dart will reveal the location of any player struck by the dart.</p>
+              </div>
+            </div>
+            <div className="d-flex mt-4">
+              <img src={reconBolt} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>RECON BOLT</h5>
+                <p className={`${styles.abilityDescription}`}>Equip a bow with recon bolt. Fire to send the bolt forward activating upon collision and revealing the location of nearby enemies caught in the line of sight of the bolt. Enemies can destroy this bolt. Hold Fire to extend the range of the projectile. Alternate Fire to add up to two bounces to this arrow.</p>
+              </div>
+            </div>
+            <div className="d-flex mt-4">
+              <img src={huntersFury} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>HUNTER'S FURY</h5>
+                <p className={`${styles.abilityDescription}`}>Equip a bow with three long-range, wall-piercing energy blasts. Fire to release an blast in a line in front of Sova dealing damage and revealing the location of enemies caught in the line. This ability can be RE-USED up to two more times while the ability timer is active.</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-5">
           <img src={sova} alt="" className={`${styles.agentImages} ${styles.slideOverFromRight}`} />
--- src/components/AgentInfos/Viper.jsx
+++ src/components/AgentInfos/Viper.jsx
@@ -1,15 +1,49 @@
 import React from "react";
 import styles from "./AgentInfos.module.css";
 import viper from "../../assets/viper.png";
+import snakeBite from "../../assets/snakeBite.png";
+import poisonCloud from "../../assets/poisonCloud.png";
+import toxicScreen from "../../assets/toxicScreen.png";
+import vipersPit from "../../assets/vipersPit.png";
 
 class Viper extends React.Component {
   render() {
     return (
       <div className={`${styles.background} ${styles.viperBackground} d-flex justify-content-around`}>
-        <div className={`col-5 mt-5 ${styles.slideOverFromLeft}`}>
+        <div className={`col-5 mt-4 ${styles.slideOverFromLeft}`}>
           <h1 className={`text-center ${styles.agentFont}`}>VIPER</h1>
           <h6 className={`text-center ${styles.agentInfoFont}`}>Viper deploys an array of poisonous chemical devices to control the battlefield and cripple the enemy’s vision. If the toxins don’t kill her prey,
           her mind games surely will.</h6>
+          <div className="mt-5">
+            <div className="d-flex">
+              <img src={snakeBite} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>SNAKE BITE</h5>
+                <p className={`${styles.abilityDescription}`}>Equip a chemical launcher. Fire to launch a canister that shatters upon hitting the floor, creating a lingering chemical zone that damages and slows enemies.</p>
+              </div>
+            </div>
+            <div className="d-flex mt-4">
+              <img src={poisonCloud} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>POISON CLOUD</h5>
+                <p className={`${styles.abilityDescription}`}>Equip a gas emitter. Fire to throw the emitter that perpetually remains throughout the round. Re-use the ability to create a toxic gas cloud at the cost of fuel. This ability can be re-used more than once and can be picked up to be redeployed.</p>
+              </div>
+            </div>
+            <div className="d-flex mt-4">
+              <img src={toxicScreen} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>TOXIC SCREEN</h5>
+                <p className={`${styles.abilityDescription}`}>Equip a gas emitter launcher. Fire to deploy a long line of gas emitters. Re-use the ability to create a tall wall of toxic gas at the cost of fuel. This ability can be re-used more than once.</p>
+              </div>
+            </div>
+            <div className="d-flex mt-4">
+              <img src={vipersPit} alt="" className={`${styles.abilities}`} />
+              <div>
+                <h5 className={`${styles.abilityName}`}>VIPER'S PIT</h5>
+                <p className={`${styles.abilityDescription}`}>Equip a chemical sprayer. Fire to spray a chemical cloud in all directions around Viper, creating a large cloud that reduces the vision range and maximum health of players inside of it.</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-5">
           <img src={viper} alt="" className={`${styles.agentImages} ${styles.slideOverFromRight}`} />
