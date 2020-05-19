b248e14bc74c97cbec5fd2d22cbb4a33c392c984
dae-hyun-kim <daehk4@uci.edu>
2020-05-19 18:51:43 +0000

    mobile responsive game info section complete for portrait mode - Dae Kim

--- src/components/GameInfoSectionOne/GameInfoSectionOne.jsx
+++ src/components/GameInfoSectionOne/GameInfoSectionOne.jsx
@@ -6,14 +6,14 @@ class GameInfoSectionOne extends React.Component {
   render() {
     return (
       <div className={styles.sectionOne}>
-        <div className="d-flex justify-content-around">
-          <div className="col-4 ml-4">
+        <div className="d-flex justify-content-around flex-wrap">
+          <div className={`col-4 ml-4 ${styles.gameInfo}`}>
             <h2 className={`${styles.slideIn} ${styles.topPadding} ${styles.bigFont}`}>DOMINATE YOUR FOES WITH PRECISION SHOOTING...  </h2>
             <div className="mt-5 col-9">
               <p className={`${styles.smallFont} ${styles.fadeIn}`}>Whether you enjoy sniping from long distances, dueling with rifles and SMGs, or blowing enemies away with shotguns, Valorant has a wide array of firearms to fit your playstyle.</p>
             </div>
           </div>
-          <div className="col-5">
+          <div className={`col-5 ${styles.mobileCenter}`}>
             <img className={`${styles.shootingGifSize} ${styles.topPadding}`} src={shootingGif} alt=""/>
           </div>
         </div>
--- src/components/GameInfoSectionTwo/GameInfoSectionTwo.jsx
+++ src/components/GameInfoSectionTwo/GameInfoSectionTwo.jsx
@@ -6,11 +6,11 @@ class GameInfoSectionTwo extends React.Component {
   render() {
     return (
       <div className={styles.sectionOne}>
-        <div className="d-flex justify-content-center">
-          <div className="col-6">
+        <div className="d-flex justify-content-center flex-wrap">
+          <div className={`col-6 ${styles.mobileSizing} ${styles.mobileCenter}`}>
             <img src={abilitiesGif} alt="" className={`${styles.shootingGifSize} ${styles.topPadding}`}/>
           </div>
-          <div className="col-4">
+          <div className={`col-4 ${styles.mobileSizing} text-right`}>
             <h2 className={` ${styles.slideIn} ${styles.topPadding} ${styles.bigFont}`}>...OR WITH STRATEGIC ABILITIES.  </h2>
             <div className="mt-5 col-11">
               <p className={`${styles.smallFont} ${styles.fadeIn}`}>Valorant has 10 agents, all with unique and specialized abilities to aid in the most intense battles, and allowing players to be creative and strategic with their approach to success.</p>
