fd9179a0db5bdab096c001928a2d2baf40129a42
dae-hyun-kim <daehk4@uci.edu>
2020-05-20 20:13:29 +0000

    mobile responsive for maps component for portrait complete - Dae Kim

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -9,15 +9,15 @@ class Maps extends React.Component {
     return(
       <div className={styles.mainBackground}>
         <div className="container d-flex flex-wrap justify-content-center">
-          <div className="col-6 mt-5 mb-5">
+          <div className={`col-6 mt-5 mb-5 ${styles.mobileSizing}`}>
             <img src={bind} alt="" className={styles.mapSize}/>
             <h2 className={`text-center mt-3 ${styles.mapNameFont}`}>BIND</h2>
           </div>
-          <div className="col-6 mt-5 mb-5">
+          <div className={`col-6 mt-5 mb-5 ${styles.mobileSizing}`}>
             <img src={haven} alt="" className={styles.mapSize} />
             <h2 className={`text-center mt-3 ${styles.mapNameFont}`}>HAVEN</h2>
           </div>
-          <div className="col-6 mt-5 mb-5">
+          <div className={`col-6 mt-5 mb-5 ${styles.mobileSizing}`}>
             <img src={split} alt="" className={styles.mapSize} />
             <h2 className={`text-center mt-3 ${styles.mapNameFont}`}>SPLIT</h2>
           </div>
